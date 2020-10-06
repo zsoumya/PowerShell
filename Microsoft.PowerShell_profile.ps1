f26d0a55dee83b5162c9dc287d70e560ac9f25fb
zsoumya <zsoumya@yahoo.com>
2020-10-06 17:05:07 -0400

    Added PS/PWSH version in the prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,6 +9,7 @@ $promptStringForeColor = 'darkred'
 $newLine = [System.Environment]::NewLine
 $bit = @{ $True = 'x64'; $False = 'x86' }[[System.Environment]::Is64BitProcess]
 $label = if ($PSVersionTable.PSEdition -eq "Core") { "PWSH" } else { "PS" }
+$label += "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)"
 $promptString = ($promptChar * ($nestedPromptLevel + 1)) + ' '
 
 $Global:GitPromptSettings.DefaultPromptPath.ForegroundColor = $promptPathForeColor
